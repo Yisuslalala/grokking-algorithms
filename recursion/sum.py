dd3da3cff553c54f93025f868a5848d559a96be6
Yisuslalala <wardodev1.00@gmail.com>
2025-05-20 00:33:19 -0600

    I upload a wrong solution lol

--- recursion/sum.py
+++ recursion/sum.py
@@ -2,21 +2,13 @@ arr = [2, 4, 3, 5]
 
 def recursive_sum(arr):
     print(f"in sum function: {arr}")
-    left_arr = arr[:1]
-    sum = [x for x in left_arr if arr[:1]]
-    print(f"sum : {sum}")
-
-    right_arr = arr[1:]
-    print(f"sliced: {right_arr}")
-    if len(arr) <= 1:
-        print("length menor o igual a 1")
-        return sum
-    else:
-        print("length más de 0")
-        return sum + recursive_sum(right_arr)
+    if len(arr) == 0:
+        return 0
+    else: 
+        return arr[0] + recursive_sum(arr[1:])
 
 def main():
-    print(sum(arr))
+    print(recursive_sum(arr))
 
 
 if __name__ == "__main__":
